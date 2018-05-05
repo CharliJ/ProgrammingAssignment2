97b502ace9d6bd97c44ed5f65a9cd4c04dfd44ec
CharliJ <38568495+CharliJ@users.noreply.github.com>
2018-05-05 08:13:04 -0400

    C2_W3 Homework: Reformatted comments again

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,19 @@
-## The functions 'makeCacheMatrix' and 'cacheSolve' work in tandem to calculate and # cache/store the inverse of a matrix.  The functions will check and see if there  # is already a cached calculation before re-calculating.  This makes it so if the  # inverse matrix is needed again later, it doesn't have to be re-calculated again, # which can be time-consuming.  Use them like this:
+## The functions 'makeCacheMatrix' and 'cacheSolve' work in tandem to calculate and
+# cache/store the inverse of a matrix.  The functions will check and see if there 
+# is already a cached calculation before re-calculating.  This makes it so if the
+# inverse matrix is needed again later, it doesn't have to be re-calculated again,
+# which can be time-consuming.  Use them like this:
 # a<- 'your matrix to be inverted'
 # b<- makeCacheMatrix(a)
 # cacheSolve(b)  (this does the original calculation)
-# cacheSolve(b)  (running it a second time on the same 'makeCacheMatrix' output 
+# cacheSolve(b)  (running it a second time on the same 'makeCacheMatrix' output
 # will retrieve the inverted matrix from cache) 
 
 
-## The 'makeCacheMatrix' accepts the matrix to be inverted as an input, and outputs #a list that's in the format required by 'cacheSolve'.  This makes it so cacheSolve #can check if there already is an inverted matrix calcaulted for the input matrix. #If so, it outputs it.  If not, it calculates a new one and stores it.
+## The 'makeCacheMatrix' accepts the matrix to be inverted as an input, and outputs
+#a list that's in the format required by 'cacheSolve'.  This makes it so cacheSolve
+#can check if there already is an inverted matrix calcaulted for the input matrix.
+#If so, it outputs it.  If not, it calculates a new one and stores it.
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -23,7 +30,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## 'cacheSolve' accepts an input in the format outputted by 'makeCacheMatrix'.  #'cacheSolve' checks to see if there is already an inverted matrix stored.  If #there is, it returns it.  If not, it calculates one, stores the result in the list #created by 'makeCacheMatrix', and prints the inverted matrix.
+## 'cacheSolve' accepts an input in the format outputted by 'makeCacheMatrix'. 
+#'cacheSolve' checks to see if there is already an inverted matrix stored.  If
+#there is, it returns it.  If not, it calculates one, stores the result in the list
+#created by 'makeCacheMatrix', and prints the inverted matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
